452a8d750ed98747041d473d96e5e34ae5e67d1b
davemurdock55 <davemurdock55@gmail.com>
2023-03-19 23:41:18 -0600

    Started actually working on the wikirealms tests

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
+import { CssBaseline } from '@nextui-org/react';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx);
+  //   return {
+  //     ...initialProps,
+  //     styles: React.Children.toArray([initialProps.styles])
+  //   };
+  // }
+
+  render() {
+    return (
+      <Html lang="en">
+        {/* <Head /> */}
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
